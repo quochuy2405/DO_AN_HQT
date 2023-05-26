7e5a5cdc81bb1f1bcc85d028c27e61e8b88c091a
quochuy2405 <huy01682312920@gmail.com>
2023-05-26 20:10:24 +0700

    update

--- components/templates/About.tsx
+++ components/templates/About.tsx
@@ -60,22 +60,20 @@ const About = () => {
                 src="/hb.png"
                 alt="Alexa featured Img"
               />
-              <p className="font-medium text-sm leading-5 text-gray-800 mt-4">Quốc Huy (founder)</p>
+              <p className="font-medium text-sm leading-5 text-gray-800 mt-4">Quốc Huy</p>
             </div>
             <div className="p-4 pb-6 flex justify-center flex-col items-center">
               <img
                 className="md:block hidden h-36 w-36 object-cover rounded-lg"
-                src="/dl.png"
+                src="/nhat.png"
                 alt="Olivia featured Img"
               />
               <img
                 className="md:hidden block h-36 w-36 object-cover rounded-lg"
-                src="/dl.png"
+                src="/nhat.png"
                 alt="Olivia featured Img"
               />
-              <p className="font-medium text-sm leading-5 text-gray-800 mt-4">
-                Diệu Linh (co-founder)
-              </p>
+              <p className="font-medium text-sm leading-5 text-gray-800 mt-4">Hoàng Nhật</p>
             </div>
           </div>
         </div>
